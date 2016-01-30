4ac0bf19f88bebc823575819fb4d7e47ffa3751f
kitman0804 <kitman0804@gmail.com>
2016-01-30 11:42:15 +0800

    v0.1 revised 'readUTF8' Now the function can read and handle files with UTF-8 character properly.

--- R/readUTF8.R
+++ R/readUTF8.R
@@ -6,16 +6,17 @@ if (!"stringr"%in%rownames(installed.packages())) install.packages("stringr")
 
 readUTF8 <- function(file, header = TRUE, sep = "\t", stringsAsFactors = FALSE) {
   x <- readLines(file)
+  x <- iconv(x, from = "UTF-8", to = "UTF-8")
   x <- stringr::str_split(x, sep)
-  header <- ifelse(header, x[[1]], NULL)
-  x <- ifelse(header, x[-1], x)
+  if (header) {
+    header <- x[[1]]
+    x <- x[-1]
+  } else {
+    header <- NULL
+    x <- x
+  }
   x <- Reduce(function(...) rbind(..., deparse.level = 0), x)
   x <- as.data.frame(x, stringsAsFactors = stringsAsFactors)
   if (!is.null(header)) names(x) <- header
-
-  x[] <- lapply(x, function(col) {
-    if (is.character(col)) Encoding(col) <- "UTF-8"
-    return(col)
-  })
   return(x)
 }
